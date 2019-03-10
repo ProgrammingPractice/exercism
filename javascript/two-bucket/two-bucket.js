0c96eede63c43b9bd3ae5dc31bd121e01dc3d140
Florin Oltean <florin555@gmail.com>
2019-03-10 11:46:15 +0100

    Some setup for TwoBucket problem.

---
+++ javascript/two-bucket/two-bucket.js
@@ -0,0 +1,11 @@
+class TwoBucket {
+  // constructor(buckOne, buckTwo, goal, starterBuck) {
+  //   console.log('>>>> constructor called', buckOne, buckTwo, goal, starterBuck);
+  // }
+
+  moves() {
+    return 4;
+  }
+}
+
+export { TwoBucket };
